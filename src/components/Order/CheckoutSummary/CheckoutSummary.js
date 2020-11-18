84ab1be485890905bac3702ffcfec839527d1171
sumit.sahu <sumit.sahu@clariontechnologies.co.in>
2020-11-18 13:59:07 +0530

    Navgating Back and to the next page(Just dummy)

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -12,10 +12,10 @@ const checkoutSummary = (props) => {
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button
-                clicked 
+                clicked={props.checkoutCancelled}
                 btnType="Danger">CANCEL</Button>
             <Button
-                clicked 
+                clicked={props.checkoutContinued}
                 btnType="Success">CONTINUE</Button>
         </div>
     )
