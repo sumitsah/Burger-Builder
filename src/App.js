6ac8d7c01d388cd2380776585982808ab103ff6a
sumit.sahu <sumit.sahu@clariontechnologies.co.in>
2020-11-18 13:48:29 +0530

    Added routing and redirect to checkout page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   state = {
@@ -18,8 +20,13 @@ class App extends Component {
     return (
       <div>
         <Layout>
+          <Switch>
           {/* { this.state.show ? <BurgerBuilder /> : null } */}
-          <BurgerBuilder />
+          {/* <BurgerBuilder />
+          <Checkout /> */}
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,28 +86,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert("You Continue!");
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Sumit Sahu',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '41351',
-                    country: 'INDIA'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false });
-                })
-            .catch(error =>{
-                this.setState({ loading: false });
-            });
+        // this.setState({ loading: true });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Sumit Sahu',
+        //         address: {
+        //             street: 'TestStreet 1',
+        //             zipCode: '41351',
+        //             country: 'INDIA'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false });
+        //         })
+        //     .catch(error =>{
+        //         this.setState({ loading: false });
+        //     });
+        this.props.history.push('/checkout');
     }
 
     render(){
